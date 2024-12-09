1c467610b1348b30a5640b5de17977d57b8ebb7d
childressg <144740845+childressg@users.noreply.github.com>
2024-12-08 21:57:32 -0600

    pop the lock - title screen

--- Projects/pop the lock/main.py
+++ Projects/pop the lock/main.py
@@ -19,8 +19,12 @@ line_angle = 0
 line_speed = 0.02
 line_direction = 1
 target_angle = math.radians(random.randrange(0, 360))
-difference_acceptance = math.radians(10) # degrees of freedom for scoring
+difference_acceptance = math.radians(9) # degrees of freedom for scoring
 score = 0
+
+#menu variables
+playing = False
+
 
 #colors
 background_color = pg.Color(79, 85, 182)
@@ -29,6 +33,15 @@ shackle_color = pg.Color(49, 49, 96)
 line_color = pg.Color(185, 16, 75)
 target_color = pg.Color(240, 207, 76)
 score_color = pg.Color(200, 201, 220)
+
+menu_button_color = pg.Color(62, 169, 241)
+menu_button_shadow_color = pg.Color(22, 64, 93)
+menu_button_hover_color = pg.Color(124, 195, 243)
+menu_button_text_color = pg.Color(203, 224, 239)
+menu_button_interact_text_color = pg.Color(116, 155, 182)
+menu_button_interact_color = pg.Color(157, 207, 241)
+title_color = pg.Color(153, 109, 212)
+title_shadow_color = pg.Color(127, 78, 192)
 
 #visual variables
 lock_radius = 100
@@ -40,6 +53,52 @@ shackle_radius = 50
 
 #fonts
 score_font = pg.font.Font("assets/grobold.ttf", 40)
+title_font = pg.font.Font("assets/grobold.ttf", 60)
+button_font = pg.font.Font("assets/grobold.ttf", 40)
+
+
+class Button:
+    def __init__(self, x, y, w, h, z, text, button_color, shadow_color, hover_color, interact_color, text_color, interact_text_color):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.z = z
+        self.text = text
+        self.button_color = button_color
+        self.shadow_color = shadow_color
+        self.hover_color = hover_color
+        self.interact_color = interact_color
+        self.text_color = text_color
+        self.interact_text_color = interact_text_color
+        self.was_down_last_frame = False
+
+    def draw(self, surface):
+        mouse_pos = pg.mouse.get_pos()
+        mouse_down = pg.mouse.get_pressed()[0]
+        if pg.Rect(self.x, self.y, self.w, self.h).collidepoint(mouse_pos[0], mouse_pos[1]):
+            if mouse_down:
+                pg.draw.rect(surface, self.interact_color, pg.Rect(self.x, self.y + self.z, self.w, self.h), border_radius=12)
+                draw_text(surface, button_font, self.text, self.interact_text_color, pg.Vector2(self.x + self.w / 2, self.y + self.h / 2 + self.z))
+                self.was_down_last_frame = True
+            else:
+                pg.draw.rect(surface, self.shadow_color, pg.Rect(self.x, self.y + self.z, self.w, self.h), border_radius=12)
+                pg.draw.rect(surface, self.hover_color, pg.Rect(self.x, self.y, self.w, self.h), border_radius=12)
+                draw_text(surface, button_font, self.text, self.text_color, pg.Vector2(self.x + self.w / 2, self.y + self.h / 2))
+                if self.was_down_last_frame:
+                    self.was_down_last_frame = False
+                    return True
+        else:
+            pg.draw.rect(surface, self.shadow_color, pg.Rect(self.x, self.y + self.z, self.w, self.h), border_radius=12)
+            pg.draw.rect(surface, self.button_color, pg.Rect(self.x, self.y, self.w, self.h), border_radius=12)
+            draw_text(surface, button_font, self.text, self.text_color, pg.Vector2(self.x + self.w / 2, self.y + self.h / 2))
+            if self.was_down_last_frame:
+                self.was_down_last_frame = False
+                return True
+        return False
+
+#buttons
+start_button = Button(200, 250, 200, 50, 10, "Start", menu_button_color, menu_button_shadow_color, menu_button_hover_color, menu_button_interact_color, menu_button_text_color, menu_button_interact_text_color)
 
 def draw_rounded_line(surface, color, start_pos, end_pos, width):
     pg.draw.line(surface, color, start_pos, end_pos, width)
@@ -59,41 +118,53 @@ def start():
     pass
 
 def update(events):
+    global playing
+
     global line_angle
     global line_speed
     global line_direction
     global target_angle
     global score
 
-    for event in events:
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_SPACE:
-                # print(line_angle - target_angle, difference_acceptance)
-                if check_angle():
-                    line_direction *= -1
-                    line_speed += 0.001
-                    target_angle = math.radians(random.randrange(0, 360))
-                    score += 1
-    #shackle
-    pg.draw.circle(screen, shackle_color, MIDDLE - pg.Vector2(0, shackle_height), shackle_radius)
-    pg.draw.circle(screen, background_color, MIDDLE - pg.Vector2(0, shackle_height), shackle_radius - shackle_thickness)
-    pg.draw.rect(screen, shackle_color, pg.Rect(MIDDLE.x - shackle_radius, MIDDLE.y - shackle_height, 2 * shackle_radius, shackle_height))
-    pg.draw.rect(screen, background_color, pg.Rect(MIDDLE.x - (shackle_radius - shackle_thickness), MIDDLE.y - shackle_height, 2 * (shackle_radius - shackle_thickness), shackle_height))
-    pg.draw.circle(screen, background_color, MIDDLE, lock_radius - lock_thickness)
 
-    #lock
-    draw_text(screen, score_font, str(score), score_color, MIDDLE)
-    pg.draw.circle(screen, lock_color, MIDDLE, lock_radius, lock_thickness)
+    if playing:
+        for event in events:
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    # print(line_angle - target_angle, difference_acceptance)
+                    if check_angle():
+                        line_direction *= -1
+                        line_speed += 0.001
+                        target_angle = math.radians(random.randrange(0, 360))
+                        score += 1
+        #shackle
+        pg.draw.circle(screen, shackle_color, MIDDLE - pg.Vector2(0, shackle_height), shackle_radius)
+        pg.draw.circle(screen, background_color, MIDDLE - pg.Vector2(0, shackle_height), shackle_radius - shackle_thickness)
+        pg.draw.rect(screen, shackle_color, pg.Rect(MIDDLE.x - shackle_radius, MIDDLE.y - shackle_height, 2 * shackle_radius, shackle_height))
+        pg.draw.rect(screen, background_color, pg.Rect(MIDDLE.x - (shackle_radius - shackle_thickness), MIDDLE.y - shackle_height, 2 * (shackle_radius - shackle_thickness), shackle_height))
+        pg.draw.circle(screen, background_color, MIDDLE, lock_radius - lock_thickness)
 
-    #gameplay
-    pg.draw.circle(screen, target_color, MIDDLE + pg.Vector2(((lock_radius + (lock_radius - lock_thickness)) / 2) * math.cos(target_angle),-((lock_radius + (lock_radius - lock_thickness)) / 2) * math.sin(target_angle)), lock_thickness * .40)
-    draw_rounded_line(screen, line_color, MIDDLE + pg.Vector2((lock_radius - lock_thickness + 5) * math.cos(line_angle), -(lock_radius - lock_thickness + 5) * math.sin(line_angle)), MIDDLE + pg.Vector2((lock_radius - 5) * math.cos(line_angle), -(lock_radius - 5) * math.sin(line_angle)), 8)
-    #debug lines
-    # pg.draw.line(screen, (255, 255, 255), MIDDLE + pg.Vector2((lock_radius - lock_thickness) * math.cos(target_angle + difference_acceptance), -(lock_radius - lock_thickness) * math.sin(target_angle + difference_acceptance)), MIDDLE + pg.Vector2(lock_radius * math.cos(target_angle + difference_acceptance), -lock_radius * math.sin(target_angle + difference_acceptance)))
-    # pg.draw.line(screen, (255, 255, 255), MIDDLE + pg.Vector2((lock_radius - lock_thickness) * math.cos(target_angle - difference_acceptance), -(lock_radius - lock_thickness) * math.sin(target_angle - difference_acceptance)), MIDDLE + pg.Vector2(lock_radius * math.cos(target_angle - difference_acceptance), -lock_radius * math.sin(target_angle - difference_acceptance)))
+        #lock
+        draw_text(screen, score_font, str(score), score_color, MIDDLE)
+        pg.draw.circle(screen, lock_color, MIDDLE, lock_radius, lock_thickness)
 
-    line_angle += line_speed * line_direction
-    line_angle = line_angle % (math.pi * 2)
+        #gameplay
+        pg.draw.circle(screen, target_color, MIDDLE + pg.Vector2(((lock_radius + (lock_radius - lock_thickness)) / 2) * math.cos(target_angle),-((lock_radius + (lock_radius - lock_thickness)) / 2) * math.sin(target_angle)), lock_thickness * .40)
+        draw_rounded_line(screen, line_color, MIDDLE + pg.Vector2((lock_radius - lock_thickness + 5) * math.cos(line_angle), -(lock_radius - lock_thickness + 5) * math.sin(line_angle)), MIDDLE + pg.Vector2((lock_radius - 5) * math.cos(line_angle), -(lock_radius - 5) * math.sin(line_angle)), 8)
+
+        #debug lines
+        # pg.draw.line(screen, (255, 255, 255), MIDDLE + pg.Vector2((lock_radius - lock_thickness) * math.cos(target_angle + difference_acceptance), -(lock_radius - lock_thickness) * math.sin(target_angle + difference_acceptance)), MIDDLE + pg.Vector2(lock_radius * math.cos(target_angle + difference_acceptance), -lock_radius * math.sin(target_angle + difference_acceptance)))
+        # pg.draw.line(screen, (255, 255, 255), MIDDLE + pg.Vector2((lock_radius - lock_thickness) * math.cos(target_angle - difference_acceptance), -(lock_radius - lock_thickness) * math.sin(target_angle - difference_acceptance)), MIDDLE + pg.Vector2(lock_radius * math.cos(target_angle - difference_acceptance), -lock_radius * math.sin(target_angle - difference_acceptance)))
+
+        line_angle += line_speed * line_direction
+        line_angle = line_angle % (math.pi * 2)
+    else:
+        draw_text(screen, title_font, "(Not) Pop the Lock", title_shadow_color, MIDDLE + pg.Vector2(0, -190))
+        draw_text(screen, title_font, "(Not) Pop the Lock", title_color, MIDDLE + pg.Vector2(0, -200))
+
+        released = start_button.draw(screen)
+        if released:
+            playing = True
 
 start()
 
